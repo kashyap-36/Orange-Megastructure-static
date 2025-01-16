193b0b0f82d6b583d1cc9da2fdf89a3d5be4c474
kashyap-36 <kashyap.chauhan@swiftrut.com>
2025-01-16 22:04:22 +0530

    dynamic footed added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
-// trial
-
+// loading footer 
+document.addEventListener("DOMContentLoaded", () => {
+    // Load Footer
+    const footerElement = document.getElementById("footer");
+    if (footerElement) {
+        fetch("/assets/components/footer.html")
+            .then((response) => response.text())
+            .then((html) => {
+                footerElement.innerHTML = html;
+                // Optional: Add footer-specific JS logic here
+            })
+            .catch((error) => console.error("Error loading footer:", error));
+    }
+});
